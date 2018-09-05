ed5498c926a3e22aeb2eeeb766fd09ed7e4c2c34
Kevin Goslar <kevin.goslar@originate.com>
2018-09-05 07:03:37 -0500

    cleanup

--- features/steps/world.ts
+++ features/steps/world.ts
@@ -1,32 +1,8 @@
 import { setWorldConstructor } from "cucumber"
-import vm from "vm"
-const debug = vm.runInDebugContext("Debug")
 
 function CustomWorld() {
   // holds the currently running search promises
   this.promises = {}
-
-  // returns the status of the given promise
-  // - {status: 'pending'}
-  // - {status: 'fulfilled', value: xxx}
-  // - {rejected: 'rejected', error: yyy}
-  this.promiseInfo = (promise: Promise<any>) => {
-    const mirror = debug.MakeMirror(promise, true)
-    if (!mirror.isPromise()) {
-      throw new Error("not a promise")
-    }
-    const status = mirror.status()
-    if (status === "pending") {
-      return { status }
-    }
-    const value = mirror.promiseValue().value()
-    if (status === "resolved") {
-      // fix terminology fuck-up
-      return { status: "fulfilled", value }
-    } else {
-      return { status, error: value }
-    }
-  }
 }
 
 setWorldConstructor(CustomWorld)
