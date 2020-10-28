e3e60aa6135ccdaa058d04efbb65dde6ccf7dba4
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-28 12:01:51 -0500

    progress

--- test/wait-for-regex-test.ts
+++ test/wait-for-regex-test.ts
@@ -35,14 +35,14 @@ test("match has already arrived when the search starts", async function () {
 test("aborting after the given timeout", async function () {
   const stream = new ReadableStream("")
   const promise = new TextStreamSearch(stream).waitForRegex(/h.*o/, 10)
-  assert.rejects(promise, new Error("Regex /h.*o/ not found within 10 ms. The captured text so far is:\n"))
+  await assert.rejects(promise, new Error("Regex /h.*o/ not found within 10 ms. The captured text so far is:\n"))
 })
 
 test("search without timeout", async function () {
   const stream = new ReadableStream("")
   const promise = new TextStreamSearch(stream).waitForRegex(/h.*o/)
   let resolved = false
-  promise.then(function () {
+  await promise.then(function () {
     resolved = true
   })
   await delay(10)
@@ -54,12 +54,12 @@ test("multiple concurrent searches", async function () {
   const search = new TextStreamSearch(stream)
   const promise1 = search.waitForRegex(/t.*1/)
   let resolved1 = false
-  promise1.then(function () {
+  await promise1.then(function () {
     resolved1 = true
   })
   const promise2 = search.waitForRegex(/t.*2/)
   let resolved2 = false
-  promise2.then(function () {
+  await promise2.then(function () {
     resolved2 = true
   })
   stream.push("text1")
@@ -76,7 +76,7 @@ test("multiple sequential searches", async function () {
   const search = new TextStreamSearch(stream)
   const promise1 = search.waitForRegex(/t.*1/)
   let resolved1 = false
-  promise1.then(function () {
+  await promise1.then(function () {
     resolved1 = true
   })
   stream.push("text1")
@@ -85,7 +85,7 @@ test("multiple sequential searches", async function () {
 
   const promise2 = search.waitForRegex(/t.*2/)
   let resolved2 = false
-  promise2.then(function () {
+  await promise2.then(function () {
     resolved2 = true
   })
   stream.push("text2")
