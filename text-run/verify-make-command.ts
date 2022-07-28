415b0426634efa2ddd8ea6caa4023cc0d4192ec7
Kevin Goslar <kevin.goslar@gmail.com>
2022-07-27 19:07:58 -0500

    verify-make-command.ts tsconfig-build.json tsconfig.json

--- text-run/verify-make-command.ts
+++ text-run/verify-make-command.ts
@@ -2,6 +2,7 @@ import { promises as fs } from "fs"
 import * as os from "os"
 import * as path from "path"
 import * as tr from "text-runner"
+import * as util from "util"
 
 export default async function (args: tr.actions.Args) {
   const expected = args.region
@@ -13,19 +14,19 @@ export default async function (args: tr.actions.Args) {
   const makefileContent = await fs.readFile(makefilePath, "utf8")
   const commands = makefileContent.split(os.EOL).filter(lineDefinesMakeCommand).map(extractMakeCommand)
   if (!commands.includes(expected)) {
-    throw new Error(`Make command "${expected}" not found in: ${commands}`)
+    throw new Error(`Make command "${expected}" not found in: ${util.inspect(commands)}`)
   }
 }
 
 // returns whether the given line from a Makefile
 // defines a Make command
-function lineDefinesMakeCommand(line: string) {
+function lineDefinesMakeCommand(line: string): boolean {
   return makeCommandRE.test(line)
 }
 const makeCommandRE = /^[^ ]+:/
 
 // returns the defined command name
 // from a Makefile line that defines a Make command
-function extractMakeCommand(line: string) {
+function extractMakeCommand(line: string): string {
   return line.split(":")[0]
 }
