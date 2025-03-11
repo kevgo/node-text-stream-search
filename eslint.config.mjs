36a22ad184608069b0723a334fbea8f1f0fdb5d8
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-11 15:55:14 -0500

    Update ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+
+import tslintPlugin from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import perfectionist from "eslint-plugin-perfectionist"
+
+export default [
+  {
+    files: ["src/*.ts"],
+    ignores: ["node_modules/", ".git/", "out/", "text-runner/"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        console: "readonly",
+        module: "readonly",
+        process: "readonly"
+      },
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json"
+      },
+      sourceType: "commonjs"
+    },
+    plugins: {
+      "@typescript-eslint": tslintPlugin
+    },
+    rules: {
+      ...tslintPlugin.configs.recommended.rules,
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "no-empty-function": "error",
+      "prefer-const": [
+        "error",
+        {
+          destructuring: "any",
+          ignoreReadBeforeAssign: false
+        }
+      ]
+    }
+  },
+  perfectionist.configs["recommended-natural"]
+]
